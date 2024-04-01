2994cc224326bf5f1c9603612107556826e28dc7
Meidencore <danieleverdale@gmail.com>
2024-04-01 14:09:10 -0400

    verifyToken middleware better error handling and bugs fixed

--- src/middlewares/verifyToken.middlewares.ts
+++ src/middlewares/verifyToken.middlewares.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import { type Request, type Response, type NextFunction } from 'express'
 import dotenv from 'dotenv'
+import { type jwtErrorResponse } from './types'
 
 // configuration of dotenv
 dotenv.config()
@@ -16,9 +17,8 @@ const secret: any = process.env.SECRETKEY
  * @returns Errors of verification or next execution
  */
 
-export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+export const verifyToken = (req: Request, res: Response, next: NextFunction): Response<jwtErrorResponse> | undefined => {
   // Check HEADER from Request for 'x-access-token'
-
   const token: any = req.headers['x-access-token']
 
   // verify if jwt is present
@@ -28,18 +28,34 @@ export const verifyToken = (req: Request, res: Response, next: NextFunction) => 
       message: 'Not Authorized to consume this endpoint'
     })
   } else {
+    // verify the token obtained
     jwt.verify(token, secret, (err: any, decoded: any) => {
-      if (err) {
-        return res.status(500).send({
+      console.log(err instanceof jwt.TokenExpiredError)
+      console.log(err instanceof jwt.JsonWebTokenError)
+
+      // Check for expired first cause err are always an instance of JsonWebTokenError and only expired are instance of TokenExpiredError
+      if (err instanceof jwt.TokenExpiredError) {
+        // Response if the token has expired
+        return res.status(401).send({
+          authenticationError: 'JWT verification failed',
+          message: 'Expired token provided'
+        })
+      } else if (err instanceof jwt.JsonWebTokenError && err.message === 'invalid token') {
+        // Response if the token is not valid
+        return res.status(400).send({
+          authenticationError: 'JWT verification failed',
+          message: 'Invalid token provided'
+        })
+      } else if (err) {
+        // throw an exception to any other error
+        return res.status(400).send({
           authenticationError: 'JWT verification failed',
           message: 'Failed to verify JWT token in request'
         })
+      } else {
+        // Execute Next function -> Protected Routes will be executed
+        next()
       }
     })
   }
-
-  // verify the token obtained
-
-  // Execute Next function -> Protected Routes will be executed
-  next()
 }
